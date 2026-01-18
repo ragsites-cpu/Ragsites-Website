fdf5f507436fdd76ce3a38dbe4f018e3c71386c3
akchaud5 <Ayushkumar.chaudhary2003@gmail.com>
2026-01-18 19:43:44 +0400

    Add Microsoft Clarity for session recordings and heatmaps

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,18 @@ export default function RootLayout({
           }}
         />
         {/* End Meta Pixel Code */}
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "v3fxwwhemk");
+          `}
+        </Script>
+
         {children}
       </body>
     </html>
