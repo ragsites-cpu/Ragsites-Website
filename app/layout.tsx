1f8e7304eaf819fa310d24a8c7dd9c6235a0179e
Sashanken <smorekonda@gmail.com>
2026-02-27 21:45:41 -0800

    added meta pixel for hood plumber campaign

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,8 +37,8 @@ export default function RootLayout({
           `}
         </Script>
 
-        {/* Meta Pixel Code */}
-        <Script id="meta-pixel" strategy="afterInteractive">
+        {/* Old Meta Pixel Code (commented out)
+        <Script id="meta-pixel-old" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
             {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -55,6 +55,28 @@ export default function RootLayout({
         <noscript
           dangerouslySetInnerHTML={{
             __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=923256480047894&ev=PageView&noscript=1" alt="" />`,
+          }}
+        />
+        End Old Meta Pixel Code */}
+
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1236892381952198');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1236892381952198&ev=PageView&noscript=1" alt="" />`,
           }}
         />
         {/* End Meta Pixel Code */}
